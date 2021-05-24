3d446bc0994c805b5cb901650194d7f5f8a70d46
alecvosika <alecvosika@comcast.net>
2021-05-23 19:10:24 -0500

    Converted all files to OOP

--- src/main.py
+++ src/main.py
@@ -3,17 +3,28 @@ from write_to_file import WriteToFile
 from get_config import *
 
 
+class Main:
+
+    def __init__(self):
+        # Gets the Client ID
+        self.client_id = GetconfigData.GetClientId()
+        # Gets the Access Token
+        self.access_token = GetconfigData.GetAccessToken()
+        # Request variable used to pull data from Oura
+        self.request = OuraClient(client_id=f'{self.client_id}',
+                                  access_token=f'{self.access_token}')
+
+
 def main():
-    client_id = GetClientId()
-    access_token = GetAccessToken()
 
-    oura = OuraClient(client_id=f'{client_id}',
-                      access_token=f'{access_token}')
+    # Create object
+    oura = Main()
 
-    who_am_i = oura.user_info()
+    # gets general user info and prints to debug console
+    who_am_i = oura.request.user_info()
     print(who_am_i, "\n")
 
-    info = oura.sleep_summary("2021-5-21")
+    info = oura.request.sleep_summary("2021-5-21")
     WriteToFile(info)
     print("Done")
 
